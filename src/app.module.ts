4794193a92ab23d7d9358c302fe1077515d115d2
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-11-28 23:53:41 +0400

    add: user, auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const env = process.env.NODE_ENV || 'development';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    AuthModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${env}`,
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTIONS_PATH, {
+      useNewUrlParser: true,
+      useUnifiebTopology: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
