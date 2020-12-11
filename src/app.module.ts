3f7f2b299cdb1447c89a1af9bcf35c4b52a34454
rebiss <barsegyanat.93@gmail.com>
2020-12-11 22:38:25 +0400

    add: mail mo, etc...

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TokenModule } from './token/token.module';
 import { configModule, connectMongo, optionMongo } from './configure.root';
+import { MailModule } from './mail/mail.module';
 @Module({
-  imports: [
-    UserModule,
-    AuthModule,
-    configModule,
-    MongooseModule.forRoot(connectMongo, optionMongo),
-    TokenModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        UserModule,
+        AuthModule,
+        configModule,
+        MongooseModule.forRoot(connectMongo, optionMongo),
+        TokenModule,
+        MailModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
